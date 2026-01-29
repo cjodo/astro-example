c6a534ef1024e5b0ac9b4d9c0183184c658df2ea
Curtis <cjododev@gmail.com>
2026-01-28 18:35:21 -0700

    use cockpitAPI class

--- src/loaders/loadFAQs.ts
+++ src/loaders/loadFAQs.ts
@@ -1,23 +1,8 @@
-// src/loaders/faq.ts
+import cockpit from "@/lib/cockpit";
+
 export type FAQ = { question: string; answer: string };
 
 export async function loadFAQs(): Promise<FAQ[]> {
-  try {
-    const res = await fetch(
-      `${import.meta.env.PUBLIC_COCKPIT_API}/content/items/faq`
-    );
-    if (!res.ok) return [];
-
-    const data = await res.json();
-
-    // If the JSON is an array already, just return it
-		console.log(data);
-    return Array.isArray(data) 
-      ? data.map((item: any) => ({ question: item.question, answer: item.answer }))
-      : [];
-  } catch (err) {
-    console.error("Error loading FAQs:", err);
-    return [];
-  }
+	const data = await cockpit.getItems("faq")
+	return data as FAQ[]
 }
-
